f7781fa241cb63d687cb8631f63822edba132c51
Dolilu <lsecret@naver.com>
2018-06-26 17:43:05 +0900

    Ch 2.2 End

--- Test/UserDaoTest.java
+++ Test/UserDaoTest.java
@@ -1,29 +1,35 @@
-import Ch1.DaoFactory;
 import Ch1.User;
 import Ch1.UserDao;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        @Test
+        public void addAndGet() throws SQLException {
+            ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+            UserDao dao = context.getBean("userDao", UserDao.class);
 
-        User user = new User();
-        user.setId("Lee");
-        user.setName("이정수");
-        user.setPassword("Spring");
+            User user = new User();
+            user.setId("Lee");
+            user.setName("이정수");
+            user.setPassword("Spring");;
 
-        dao.add(user);
+            dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
+            User user2 = dao.get(user.getId());
 
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
+            assertThat(user2.getName(), is(user.getName()));
+            assertThat(user2.getPassword(), is(user.getPassword()));
+        }
 
-        System.out.println(user2.getId() + " 조회 성공");
-    }
+        public static void main(String[] args) {
+            JUnitCore.main("UserDaoTest");
+        }
 }
