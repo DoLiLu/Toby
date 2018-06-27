72011e2f2e60ddeac294afc8135554bae77050c6
Dolilu <lsecret@naver.com>
2018-06-27 16:56:09 +0900

    Ch 3.2 End

---
+++ src/Book1/StatementStrategy.java
@@ -0,0 +1,9 @@
+package Book1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
