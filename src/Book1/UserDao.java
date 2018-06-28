39396a44a4d5b55024c8328c6941bcff961fd32b
Dolilu <lsecret@naver.com>
2018-06-28 19:45:29 +0900

    Ch 3.4 End

--- src/Book1/UserDao.java
+++ src/Book1/UserDao.java
@@ -11,22 +11,27 @@ import java.sql.SQLException;
 
 public class UserDao {
     private DataSource dataSource;
+    private JdbcConText jdbcConText;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.jdbcConText = new JdbcConText();
+        this.jdbcConText.setDataSource(dataSource);
     }
 
+
     public void add(final User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement(
-                        "insert into users(id, name, password) values(?,?,?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-                return ps;
+        this.jdbcConText.workWithStatementStrategy(
+            new StatementStrategy() {
+                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                    ps.setString(1, user.getId());
+                    ps.setString(2, user.getName());
+                    ps.setString(3, user.getPassword());
+                    return ps;
+                }
             }
-        });
+        );
     }
 
 
@@ -56,7 +61,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy(
+        this.jdbcConText.workWithStatementStrategy(
             new StatementStrategy() {
                 public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                     PreparedStatement ps = c.prepareStatement("delete from users");
@@ -66,23 +71,23 @@ public class UserDao {
         );
     }
 
-    private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try{
-            c = dataSource.getConnection();
-
-            ps =stmt.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e){
-            throw e;
-        } finally {
-            if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
-            if (c != null) { try {c.close(); } catch (SQLException e) {} }
-        }
-    }
+//    private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+//        Connection c = null;
+//        PreparedStatement ps = null;
+//
+//        try{
+//            c = dataSource.getConnection();
+//
+//            ps =stmt.makePreparedStatement(c);
+//
+//            ps.executeUpdate();
+//        } catch (SQLException e){
+//            throw e;
+//        } finally {
+//            if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
+//            if (c != null) { try {c.close(); } catch (SQLException e) {} }
+//        }
+//    }
 
 
     public int getCount() throws SQLException  {
