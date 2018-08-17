3e3cfb69df33787a986ef3c71cdf2e7fb618be9a
Dolilu <lsecret@naver.com>
2018-08-17 10:22:31 +0900

    Ch 6.6 End

--- src/Book1/service/UserService.java
+++ src/Book1/service/UserService.java
@@ -2,7 +2,13 @@ package Book1.service;
 
 import Book1.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     void add(User user);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    void update(User user);
     void upgradeLevels();
 }
