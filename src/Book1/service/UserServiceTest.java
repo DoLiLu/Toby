57d1e2aa4ef2dcc85c4f66439278d1d0f235de6d
Dolilu <lsecret@naver.com>
2018-08-17 10:39:18 +0900

    Ch 6 End

--- src/Book1/service/UserServiceTest.java
+++ src/Book1/service/UserServiceTest.java
@@ -21,6 +21,10 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -207,6 +211,14 @@ public class UserServiceTest {
     @Test(expected= TransientDataAccessResourceException.class)
     public void readOnlyTransactionAttribute() {
         testUserService.getAll();
+    }
+
+    @Test
+    @Transactional(propagation= Propagation.NEVER)
+    public void transactionSync() {
+        userService.deleteAll();
+        userService.add(users.get(0));
+        userService.add(users.get(1));
     }
 
     static class TestUserServiceImpl extends UserServiceImpl {
